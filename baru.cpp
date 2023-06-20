dfb62ef9b1d0691c147b4b3cef02db367bb967f2
Futonomi <andromedanewww@gmail.com>
2023-06-20 18:41:07 +0700

    ikan

---
+++ baru.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void terbang();
+int main(){
+    terbang();
+    cout<<"Falih"<<endl;
+    cout<<6+8<<endl;
+}
+
+void terbang(){
+    cout<<"gurame"<<endl;
+
+}
